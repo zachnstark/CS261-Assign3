6cd486fd85aac30d0a25d20acf6aca48586d63ca
Zachary Stark <starkz@flip3.engr.oregonstate.edu>
2015-01-25 01:29:06 -0800

    Homework 3 starting files

---
+++ testLinkedList.c
@@ -0,0 +1,14 @@
+#include "linkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+           
+
+   	/* Test your linked list in here! */
+
+   	printf("Hello from test code!\n");
+		return 0;
+}
+
+
