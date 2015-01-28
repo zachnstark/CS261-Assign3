9cc37d85ed3f031f1a58f19bb26f8fe68ad2d4a2
Kyle Nichols <nichokyl@onid.oregonstate.edu>
2015-01-28 13:37:41 -0800

    Added part 3

---
+++ part3/testCirListDeque.c
@@ -0,0 +1,14 @@
+#include "cirListDeque.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+        
+
+	/* Test your linked list in here! */
+
+	printf("Hello from test code!\n");
+	return 0;
+}
+
+
